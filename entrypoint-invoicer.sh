82dcd894f54babe50252ecd92077fcf8b50ab6b5
nolim1t <hello@nolim1t.co>
2019-01-09 12:25:04 +0700

    Checkpoint working

--- entrypoint-invoicer.sh
+++ entrypoint-invoicer.sh
@@ -1,4 +1,3 @@
 
-echo "connecting to $(LNDHOST)"
 /bin/invoicer -lnd-host=$LNDHOST -lnd-invoice=/lnd/data/chain/bitcoin/mainnet/invoice.macaroon -lnd-readonly=/lnd/data/chain/bitcoin/mainnet/readonly.macaroon -mainnet -lnd-tls=/lnd/tls.cert
 
