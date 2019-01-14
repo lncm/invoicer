01540f4d2fc1e0d4df27736d29f526d8f7ab4ffa
nolim1t <hello@nolim1t.co>
2019-01-15 00:05:25 +0700

    Fix up entrypoint file

--- entrypoint-invoicer.sh
+++ entrypoint-invoicer.sh
@@ -1,7 +1,53 @@
 #!/usr/bin/env bash
 
-/bin/invoicer -lnd-host=$LNDHOST \
-    -lnd-invoice=/lnd/data/chain/bitcoin/mainnet/invoice.macaroon \
-    -lnd-readonly=/lnd/data/chain/bitcoin/mainnet/readonly.macaroon \
-    -lnd-tls=/lnd/tls.cert \
-    -static-dir=/static/
+# Default values
+if [ -z $LNCLIENT ]; then
+    LNCLIENT='lnd'
+fi
+if [ -z $BTCHOST ]; then
+    BTCHOST='btcbox'
+fi
+if [ -z $BTCRPCUSER ]; then
+    BTCRPCUSER='invoicer'
+fi
+if [ -z $BTCRPCPASS ]; then
+    BTCRPCPASS='password'
+fi
+if [ -z $BTCRPCPORT ]; then
+    BTCRPCPORT=8332
+fi
+
+if [ -z $LNDHOST ]; then
+    LNDHOST='lightningbox'
+fi
+if [ -z $LNDPORT ]; then
+    LNDPORT=10009
+fi
+if [ -z $PORT ]; then
+    PORT=8080
+fi
+if [ -z $READONLYMACAROON ]; then
+    READONLYMACAROON='/lnd/data/chain/bitcoin/mainnet/readonly.macaroon'
+fi
+if [ -z $INVOICEMACAROON ]; then
+    INVOICEMACAROON='/lnd/data/chain/bitcoin/mainnet/invoice.macaroon'
+fi
+if [ -z $TLSFILE ]; then
+    LNDTLSFILE='/lnd/tls.cert'
+fi
+if [ -z $STATICDIR ]; then
+    STATICDIR='/static/'
+fi
+
+/bin/invoicer -ln-client=$LNCLIENT \
+    -lnd-host=$LNDHOST \
+    -lnd-port=$LNDPORT \
+    -port=$PORT \
+    -lnd-invoice=$INVOICEMACAROON \
+    -lnd-readonly=$READONLYMACAROON \
+    -lnd-tls=$TLSFILE \
+    -bitcoind-host=$BTCHOST \
+    -bitcoind-user=$BTCRPCUSER \
+    -bitcoind-pass=$BTCRPCPASS \
+    -bitcoind-port=$BTCRPCPORT \
+    -static-dir=$STATICDIR
