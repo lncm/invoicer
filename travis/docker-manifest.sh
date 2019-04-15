b7e546f861f5c0fd037ed7f6ffe89550b82d1526
Damian Mee <spam@meedamian.com>
2019-04-15 20:42:25 +0700

    Hopefully that fixes Docker…

--- travis/docker-manifest.sh
+++ travis/docker-manifest.sh
@@ -17,25 +17,23 @@ docker version
 
 
 IMAGE_VERSIONED="${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}"
-IMAGE_VER_AMD64="${IMAGE_VERSIONED}-linux-amd64"
-IMAGE_VER_ARM="${IMAGE_VERSIONED}-linux-arm"
+IMAGE_AMD64="${IMAGE_VERSIONED}-linux-amd64"
+IMAGE_ARM="${IMAGE_VERSIONED}-linux-arm"
 
 
-docker pull "${IMAGE_VER_AMD64}"
-docker pull "${IMAGE_VER_ARM}"
+docker pull "${IMAGE_AMD64}"
+docker pull "${IMAGE_ARM}"
 
 
 echo     "Pushing manifest ${IMAGE_VERSIONED}"
-docker -D manifest create "${IMAGE_VERSIONED}"  "${IMAGE_VER_AMD64}"  "${IMAGE_VER_ARM}"
-docker manifest annotate  "${IMAGE_VERSIONED}"  "${IMAGE_VER_ARM}"  --os linux --arch arm --variant v6
+docker -D manifest create "${IMAGE_VERSIONED}"  "${IMAGE_AMD64}"  "${IMAGE_ARM}"
+docker manifest annotate  "${IMAGE_VERSIONED}"  "${IMAGE_ARM}"  --os linux --arch arm --variant v6
 docker manifest push      "${IMAGE_VERSIONED}"
 
 
 IMAGE_LATEST="${TRAVIS_REPO_SLUG}:latest"
-IMAGE_LATEST_AMD64="${IMAGE_LATEST}-linux-amd64"
-IMAGE_LATEST_ARM="${IMAGE_LATEST}-linux-arm"
 
 echo     "Pushing manifest ${IMAGE_LATEST}"
-docker -D manifest create "${IMAGE_LATEST}"  "${IMAGE_LATEST_AMD64}"  "${IMAGE_LATEST_ARM}"
-docker manifest annotate  "${IMAGE_LATEST}"  "${IMAGE_LATEST_ARM}"  --os linux --arch arm --variant v6
-docker manifest push "${IMAGE_LATEST}"
+docker -D manifest create "${IMAGE_LATEST}"  "${IMAGE_AMD64}"  "${IMAGE_ARM}"
+docker manifest annotate  "${IMAGE_LATEST}"  "${IMAGE_ARM}"  --os linux --arch arm --variant v6
+docker manifest push      "${IMAGE_LATEST}"
