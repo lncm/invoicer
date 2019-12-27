fe9ae6034b7699735982881ef5fcf6ec620a697c
Damian Mee <spam@meedamian.com>
2019-12-27 23:27:37 +0700

    fix base arch selection

--- scripts/ci-set-target-image-architecture.sh
+++ scripts/ci-set-target-image-architecture.sh
@@ -38,6 +38,9 @@ if command -v gsed >/dev/null; then
   SED=gsed
 fi
 
+##
+## APPROACH #1 (new):  Use `--platform=` flag in the final `FROM` directive
+##
 # Decyphering `sed` expressions is always "fun"…  So to make it easier on the reader here's an explanation:
 # tl;dr: Replace last FROM with one that specifies target CPU architecture.
 #
@@ -51,10 +54,45 @@ fi
 # Note:
 #   `-i` - apply changes in-place (actually change the file)
 #   `s/` - substitute; followed by two `|`-separated sections:
-#     1st section looks for a match.  Escaped \( \) define a _capture group_
+#     1st section looks for a match.  Escaped \(\) define a _capture group_
 #     2nd section defines replacement.  `\1` is the value of the _capture group_ from the 1st section
-${SED} -i "s|^FROM \(.*final\)$|FROM --platform=linux/$CPU \1|" "${FILE}"
+#
+platform() {
+  # Convert matrix-supplied architecture to a format understood by Docker's `--platform=`
+  case "$1" in
+  arm32v6)  CPU="arm/v6" ;;
+  arm32v7)  CPU="arm/v7" ;;
+  arm64)    CPU="arm64"  ;;
+  esac
 
-echo "Dockerfile modified: CPU architecture of the final stage set to: ${CPU}"
+  ${SED} -i "s|^FROM \(.*final\)$|FROM --platform=linux/$CPU \1|" "${FILE}"
+}
+
+
+##
+## APPROACH #2 (old):  Use platform prefix for the image name
+##
+# This one does very similar things to the example above, except it uses the "raw" $ARCH, and
+# this is the permutation occuring:
+#
+#   FROM       alpine:3.10 AS final
+#     ⬇             ⬇           ⬇
+#   FROM armv7/alpine:3.10 AS final
+prefix() {
+  CPU="$1"
+  if [[ "${CPU}" == "arm64" ]]; then
+    CPU="arm64v8"
+  fi
+
+  ${SED} -i "s|^FROM \(.*final\)$|FROM $CPU/\1|" "${FILE}"
+}
+
+## NOTE: Currently commented out, as it doesn't seem o work yet…
+#platform "${ARCH}"
+
+# TODO: fix `--platorm=`
+prefix "${ARCH}"
+
+echo "Dockerfile modified: 'final' stage is now:"
 
 grep '^FROM.*final$' "${FILE}"
