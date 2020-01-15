80da2d7cd4477061105892fa9d8fd9f542f98257
Damian Mee <spam@meedamian.com>
2020-01-16 02:46:47 +0700

    Style changes to new-release.sh

--- scripts/new-release.sh
+++ scripts/new-release.sh
@@ -3,30 +3,32 @@
 set -eo pipefail
 
 #
-## Given version (and optional variant), this script creates & pushes a relevant git-tag.
+## Given version, this script creates & pushes a relevant git-tag.
 #
 
 # required version
 VERSION=$1
 
-# Verify version to-be-released is provided
+# Make sure ${VERSION} is provided before proceeding
 if [[ -z "${VERSION}" ]]; then
   >&2 printf "\nERR: version missing:  version needs to be passed as the first argument.  Try:\n"
   >&2 printf "\t./%s  %s\n\n"   "$(basename "$0")"  "v0.7.2"
   exit 1
 fi
 
-# Verify there's no uncommitted changes in the working dir
+# Verify there's no uncommitted changes
 if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
   >&2 printf "\nERR: working directory not clean.  Commit, or stash changes to continue.\n\n"
   exit 1
 fi
 
+# Make sure specified ${VERSION} is present in Dockerfile
 if ! grep -q "${VERSION}" "./Dockerfile" ; then
   >&2 printf "\nERR: Requested version not present in Dockerfile. Make sure that's what you want to do.\n\n"
   exit 1
 fi
 
+# Update git-tags from the remote
 git fetch --tags
 
 # Get last build number
@@ -35,17 +37,13 @@ LAST=$(git tag | grep '+build' | sed 's|^.*build||' | sort -h | tail -n 1)
 # Increment it
 ((LAST++))
 
+# Construct the full ${TAG}, ex: `v0.7.7+build666`
 TAG="${VERSION}+build${LAST}"
 
 printf "Creating tag: %s…\t" "${TAG}"
-
 git tag -sa "${TAG}" -m "${TAG}"
-
 echo "done"
 
-
 printf "Pushing tag: %s…\t" "${TAG}"
-
 git push origin "${TAG}"
-
 echo "All done"
